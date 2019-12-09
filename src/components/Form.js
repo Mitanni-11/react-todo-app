f238bf48145c650ed702f6b4475c01fc941b510d
Hironori Mitanie <sangudadian@gmail.com>
2019-12-09 21:58:59 +0900

    make layout&& make Form component

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Form extends React.Component{
+    render(){
+        return(
+            <form>
+                <input type="text"/>
+                <button>追加</button>
+            </form>
+        );
+    }
+}
+export default Form;
